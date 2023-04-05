ef80772813e477fb42a98ab56b6c5f586b2081c2
benbogd4n <cohos.sweat.0c@icloud.com>
2023-04-05 14:29:21 +0200

    added models

--- djangoapp/weatherapp/models.py
+++ djangoapp/weatherapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Weather(models.Model):
+    select = models.CharField(max_length=200)
+    # image = models.ImageField()
+    result = models.TextField()
+
+#with evey new model 2 steps:
+    #╰─ python3 manage.py makemigrations
+    #╰─ python3 manage.py migrate
+
+def __str__(self):
+    return self.select
